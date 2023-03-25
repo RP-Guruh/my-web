d91bd8c28162bd6643c62f4fe1bb3c704ecd9845
rp-guruh <github.gurh@gmail.com>
2023-03-25 13:57:32 +0700

    make button component

--- components/Button.vue
+++ components/Button.vue
@@ -1,11 +1,46 @@
 <template>
-   
-        <button class="pushable">
-            <span class="shadow"></span>
-            <span class="edge"></span>
-            <span class="front">
-                Education
-            </span>
-        </button>
-    
+    <div class="flex justify-center items-center">
+        <button type="button" class="w-40 btn text-slate-700 font-bold">{{ title }}</button>
+    </div>
 </template>
+
+<script setup>
+
+const props = defineProps({
+    title: String,
+    marginTop: String
+});
+</script>
+
+
+<style scoped>
+.btn {
+    
+    border: 3px solid black;
+    padding: 10px;
+    margin: 30px;
+    background-color: white;
+    border-radius: 10px;
+    box-shadow: 3px -3px black;
+}
+
+.btn:hover {
+    transform: translate(-3px, 3px);
+    box-shadow: -3px 3px white;
+
+}
+
+input,
+textarea,
+button,
+select,
+a {
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+}
+a:focus,a:visited,a:active{
+  outline: none;
+}
+* {
+    -webkit-tap-highlight-color: transparent;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -8,55 +8,18 @@
         <!-- Perkenalan -->
         <IntroComponent></IntroComponent>
 
-        <div class="flex justify-center items-center mt-4">
-            <button type="button" class="w-40 btn text-slate-700 font-bold">Pendidikan</button>
+        <div v-for="item in valueButton">
+            <Button :title="item"></Button>
         </div>
-
-        <div class="flex justify-center items-center mt-2">
-            <button type="button" class="w-40 btn text-slate-700 font-bold">Keahlian</button>
-        </div>
-
-        <div class="flex justify-center items-center mt-2">
-            <button type="button" class="w-40 btn text-slate-700 font-bold">Kontak Saya</button>
-        </div>
-
-        <div class="flex justify-center items-center mt-2">
-            <button type="button" class="w-40 btn text-slate-700 font-bold">Blog</button>
-        </div>
+       
         
         <ChangeMode></ChangeMode>
         
     </div>
 </template>
 
-<style scoped>
-.btn {
-    
-    border: 3px solid black;
-    padding: 10px;
-    margin: 30px;
-    background-color: white;
-    border-radius: 10px;
-    box-shadow: 3px -3px black;
-}
+<script setup lang="ts">
+    const valueButton: string[] = ["Education", "Skills", "Contact Me", "Portofolio", "Blog"];
+   
+</script>
 
-.btn:hover {
-    transform: translate(-3px, 3px);
-    box-shadow: -3px 3px white;
-
-}
-
-input,
-textarea,
-button,
-select,
-a {
-  -webkit-tap-highlight-color: rgba(0,0,0,0);
-}
-a:focus,a:visited,a:active{
-  outline: none;
-}
-* {
-    -webkit-tap-highlight-color: transparent;
-}
-</style>
